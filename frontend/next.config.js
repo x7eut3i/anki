062916f72dbc66db37ea7958d0ec33de083e29de
x7eut3i <x7eut3i@gmail.com>
2026-03-05 17:43:52 +0800

    remove linting

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true, // required for static export
   },
+  // Skip in production build (run separately in dev/CI)
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 module.exports = nextConfig;
